89dcbeb3e9624aaafdeb87f94431d1de908ddf3a
Paul Rohorzka <paul@paulroho.com>
2018-12-24 07:11:19 +0100

    Extract getter

--- Greety/Dmz/Program.cs
+++ Greety/Dmz/Program.cs
@@ -8,9 +8,11 @@ namespace Greety.Dmz
         public static void Main()
         {
             var greeter = new Greeter();
-            var name = greeter.AskForName(OutputAction, () => Console.ReadLine());
+            var name = greeter.AskForName(OutputAction, InputAction);
             greeter.Greet(name, OutputAction);
         }
+
+        private static Func<string> InputAction => Console.ReadLine;
 
         public static Action<string> OutputAction => Console.WriteLine;
     }
