bc7abcf37c1d4d17f96d1ec093ae319f73f8773c
Paul Rohorzka <paul@paulroho.com>
2019-01-04 02:21:57 +0100

    Introduce Kernel (no other changes)

---
+++ Greety/HappyZone/Kernel.cs
@@ -0,0 +1,20 @@
+﻿namespace Greety.HappyZone
+{
+    public class Kernel
+    {
+        private readonly IInputOutput _inOut;
+
+        public Kernel(IInputOutput inOut)
+        {
+            _inOut = inOut;
+        }
+
+        public void Run()
+        {
+            var greeter = new Greeter(_inOut, new[] { "Paul" });
+
+            var name = greeter.AskForName();
+            greeter.Greet(name);
+        }
+    }
+}
